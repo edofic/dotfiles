7bc39278a961e2aea74c1dd2aa98d4b0930b6d89
Andraz Bajt <andraz@bajt.me>
2016-05-02 09:03:36 +0200

    Revert hardware conf

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
