ed0b4536537a4a3dedb3fa5b9a681a24ab2fcf24
Andraz Bajt <andraz@bajt.me>
2016-12-10 21:56:45 +0100

    Fix shebang

--- .xmonad/random_imgur_wallpaper.sh
+++ .xmonad/random_imgur_wallpaper.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 URL=$( curl "http://imgur.com/r/catwallpapers/new" | egrep -o "i.imgur.com/[[:alnum:]]+" | cut -c-19 | shuf | head -n1 ).jpg
-ID=$( echo $URL | cut -c13-19 ) 
+ID=$( echo $URL | cut -c13-19 )
 wget -O /tmp/$ID.jpg $URL
 
 cd ~/.xmonad
